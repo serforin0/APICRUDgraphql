5c9d00b6d876e0fe61395e0d0e49078a3724b976
serforin <serforin@gmail.com>
2020-11-23 12:37:37 -0400

    create select
    resolve_all_student
    resolve_student
    resolve_all_degree
    Querying a list of students, single student, list of degree

--- students/students/apps/usuarios/models.py
+++ students/students/apps/usuarios/models.py
@@ -1,6 +1,18 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
-# Create your models here.
+
 class Student(models.Model):
-	name = models.CharField(max_length=45)
+	name = models.CharField(max_length=50, unique=True)
 	age = models.IntegerField()
+	degree = models.ManyToManyField("degree", blank=True)
+	date_created = models.DateTimeField(auto_now=False, auto_now_add=False, blank=True, null=True)
+
+	def __str__(self):
+		return self.name
+
+class Degree(models.Model):
+	name = models.CharField(max_length=50, unique=True)
+
+	def __str__(self):
+		return self.name
--- students/students/apps/usuarios/schema.py
+++ students/students/apps/usuarios/schema.py
@@ -1,15 +1,43 @@
 import graphene
+from graphene import Argument
 from graphene_django import DjangoObjectType
 from .models import *
+
 
 
 class StudentType(DjangoObjectType):
 	class Meta:
 		model = Student
 
+class DegreeType(DjangoObjectType):
+	class Meta:
+		model =  Degree
+
 
 class Query(graphene.ObjectType):
-	students = graphene.List(StudentType)
-	
-	def resolve_students(self, info, **kwargs):
+	all_student = graphene.List(StudentType)
+	student = graphene.Field(StudentType, id=graphene.ID())
+
+	all_degree =  graphene.List(DegreeType)
+	degree = graphene.Field(DegreeType, id=graphene.ID())
+
+	def resolve_all_student(self, info, **kwargs):
+		# Querying a list of students
 		return Student.objects.all()
+
+	def resolve_student(self, info, id):
+		#Querying a single student
+		return Student.objects.get(pk=id)
+
+	def resolve_all_degree(self, info, **kwargs):
+		#Querying a list of degree
+		return Degree.objects.all()
+
+
+
+
+# class Query(graphene.ObjectType):
+# 	students = graphene.List(StudentType)
+	
+# 	def resolve_students(self, info, **kwargs):
+# 		return Student.objects.all()
