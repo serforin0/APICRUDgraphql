3a72b3e5ab124f300a290705bdfa36afcedf273d
serforin <serforin@gmail.com>
2020-11-24 01:45:39 -0400

    docker
    add docker and modific frontend

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GraphQLModule } from './graphql.module';
 import { HttpClientModule } from '@angular/common/http';
@@ -11,6 +12,7 @@ import { HttpClientModule } from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     GraphQLModule,
     HttpClientModule
   ],
--- frontend/src/app/graphql.module.ts
+++ frontend/src/app/graphql.module.ts
@@ -1,18 +1,17 @@
-import { NgModule } from "@angular/core";
-import { ApolloModule, APOLLO_OPTIONS } from "apollo-angular";
-import { HttpLinkModule, HttpLink } from "apollo-angular-link-http";
-import { InMemoryCache } from "apollo-cache-inmemory";
+import {NgModule} from '@angular/core';
+import {APOLLO_OPTIONS} from 'apollo-angular';
+import {ApolloClientOptions, InMemoryCache} from '@apollo/client/core';
+import {HttpLink} from 'apollo-angular/http';
 
-const uri = "http://localhost:3000/graphql"; // <-- add the URL of the GraphQL server here
-export function createApollo(httpLink: HttpLink) {
+const uri = 'http://127.0.0.1:8000/graphql/'; // <-- add the URL of the GraphQL server here
+export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
   return {
-    link: httpLink.create({ uri }),
+    link: httpLink.create({uri}),
     cache: new InMemoryCache(),
   };
 }
 
 @NgModule({
-  exports: [ApolloModule, HttpLinkModule],
   providers: [
     {
       provide: APOLLO_OPTIONS,
